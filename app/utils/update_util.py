4ff94a796ffdbab5b1d210d8e88f92a9381fbfd5
BranBPDev <bouzaspradobran@gmail.com>
2026-02-18 23:09:30 +0100

    Auto updater completamente funcional

--- app/utils/update_util.py
+++ app/utils/update_util.py
@@ -1,66 +1,49 @@
 import os
-import shutil
 import subprocess
 import sys
-import ctypes
-from ctypes import wintypes
-
+import shutil
 from app.utils.json_util import read_json
 from app.utils.download_util import download_files
 from app.utils.zip_util import unzip_file
 from app.utils.paths_util import (
-    VERSION_JSON,
-    REMOTE_VERSION_JSON,
-    LATEST_ZIP_URL,
-    TEMP_ZIP_PATH,
-    DOWNLOAD_FOLDER,
-    EXECUTABLE_PATH,
-    BASE_DIR
+    VERSION_JSON, REMOTE_VERSION_JSON, LATEST_ZIP_URL, 
+    TEMP_ZIP_PATH, DOWNLOAD_FOLDER, BASE_DIR
 )
 
-def is_latest_version() -> bool:
+def is_latest_version():
     return read_json(VERSION_JSON)["version"] == read_json(REMOTE_VERSION_JSON)["version"]
 
 def perform_update():
+    # 1. Limpieza preventiva
+    if DOWNLOAD_FOLDER.exists():
+        shutil.rmtree(DOWNLOAD_FOLDER)
     DOWNLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
 
-    download_files(urls=(LATEST_ZIP_URL,), paths=(TEMP_ZIP_PATH,))
-
+    # 2. Descarga solo del ZIP
+    download_files((LATEST_ZIP_URL,), (TEMP_ZIP_PATH,))
+    
+    # 3. Descomprimir
     unzip_file(TEMP_ZIP_PATH, DOWNLOAD_FOLDER)
 
-    subprocess.Popen([sys.executable, "-c", "from app.utils.update_util import _update_subprocess; _update_subprocess(%d)" % os.getpid()], shell=False)
+    exe_name = os.path.basename(sys.executable)
+    pid = os.getpid()
 
-    sys.exit()
+    # 4. Comando de CMD
+    # Cambiamos 'start' por una llamada directa que fuerce una nueva consola.
+    # El uso de 'conhost' o 'cmd /c start' asegura que se asigne una ventana.
+    cmd_chain = (
+        f'title Actualizador && :loop && tasklist /fi ""pid eq {pid}"" | findstr {pid} >nul && (timeout /t 1 /nobreak >nul & goto loop) || '
+        f'(for /d %x in ("{BASE_DIR}\\*") do (if /i not "%~nx"=="{DOWNLOAD_FOLDER.name}" rd /s /q "%x")) && '
+        f'(for %x in ("{BASE_DIR}\\*") do (if /i not "%~nx"=="{DOWNLOAD_FOLDER.name}" del /f /q "%x")) && '
+        f'xcopy /e /y /h "{DOWNLOAD_FOLDER}\\*" "{BASE_DIR}\\" >nul 2>&1 && '
+        f'rd /s /q "{DOWNLOAD_FOLDER}" && '
+        f'explorer.exe ""{BASE_DIR}\\{exe_name}""'
+    )
 
-def _wait_process_windows(pid: int):
-    SYNCHRONIZE = 0x00100000
-    INFINITE = 0xFFFFFFFF
-
-    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-
-    handle = kernel32.OpenProcess(SYNCHRONIZE, False, pid)
-    if not handle:
-        return
+    subprocess.Popen(
+        f'cmd.exe /c "{cmd_chain}"',
+        creationflags=0x08000000, 
+        shell=True
+    )
     
-    kernel32.WaitForSingleObject(handle, INFINITE)
-    kernel32.CloseHandle(handle)
-
-def _update_subprocess(main_pid: int):
-
-    _wait_process_windows(main_pid)
-
-    for item in EXECUTABLE_PATH.parent.iterdir():
-        if item == DOWNLOAD_FOLDER:
-            continue
-        item.unlink() if item.is_file() else shutil.rmtree(item)
-
-    for item in DOWNLOAD_FOLDER.iterdir():
-        if item.is_file():
-            shutil.copy2(item, EXECUTABLE_PATH.parent / item.name)
-        else:
-            shutil.copytree(item, EXECUTABLE_PATH.parent / item.name)
-
-    shutil.rmtree(DOWNLOAD_FOLDER, ignore_errors=True)
-
-    subprocess.Popen([str(EXECUTABLE_PATH)], shell=False)
     sys.exit()
